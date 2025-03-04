d40c1558245b69a5f874e3c4f3d77b6b6f2ed243
Chalo Salvador <chalo@monogram.io>
2025-03-04 17:11:19 +0100

    Test deploy action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+				TEST DEPLOY PREVIEW
         <Image
           className={styles.logo}
           src="/next.svg"
